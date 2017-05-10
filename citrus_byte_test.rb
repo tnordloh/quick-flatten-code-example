8501a8622cb68a261c84a267db0cb420035b9ed4
Timothy Nordloh <tnordloh@gmail.com>
2017-05-10 11:33:32 -0600

    make class checks consistent

--- citrus_byte_test.rb
+++ citrus_byte_test.rb
@@ -5,13 +5,19 @@ require_relative 'citrus_byte.rb'
 describe ArrayTool do
 
   it "can flatten an array of integers" do
-    input = [[1,2,[3]],4]
+    input  = [[1,2,[3]],4]
     result = [1,2,3,4]
     ArrayTool.flatten(input).must_equal(result)
   end
 
   it "makes sure an empty array returns an empty array" do
     ArrayTool.flatten([]).must_equal([])
+  end
+
+  it "can flatten a deeply nested array" do
+    input  = [[[[[12345]]]]]
+    result = [12345]
+    ArrayTool.flatten(input).must_equal(result)
   end
 
   it "raises an error on anything other than an integer" do
