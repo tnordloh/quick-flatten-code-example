c22345cfd2f321f6ee0ad0b4d9da408af7214498
Timothy Nordloh <tnordloh@gmail.com>
2017-05-10 16:25:41 -0600

    Edit comments to test

--- citrus_byte_test.rb
+++ citrus_byte_test.rb
@@ -28,7 +28,9 @@ describe ArrayTool do
   it "can demonstrate Ruby's recursion weaknesses" do
     #This isn't a 'real' test.  Its purpose is to demonstrate the
     #circumstances that cause the current implementation of 
-    #ArrayTool.flatten to fail.
+    #ArrayTool.flatten to fail. See README.md for an alternative, which
+    #could be used in the unlikely event that we need to process an array that
+    #is nested to a depth in the thousands.
     
     input = (1..100_000).reduce([1]) { |acc,_| [acc] }
     proc { ArrayTool.flatten(input) }.must_raise(SystemStackError)
