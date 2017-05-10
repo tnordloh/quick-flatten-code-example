da2b7a36f3b817c5c6d326501a95bbe1ab36d2a9
Timothy Nordloh <tnordloh@gmail.com>
2017-05-10 12:43:24 -0600

    make class checks consistent

--- citrus_byte_test.rb
+++ citrus_byte_test.rb
@@ -26,6 +26,7 @@ describe ArrayTool do
   end
 
   it "can demonstrate Ruby's recursion weaknesses" do
+    skip
     input = (1..100_000).reduce([1]) { |acc,_| [acc] }
     ArrayTool.flatten(input).must_equal([1])
   end
